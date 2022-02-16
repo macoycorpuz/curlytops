a6639b12802e9672602d57e6fff6d7366c264277
Marcuz Corpuz <macoycorpuz@gmail.com>
2022-02-16 18:49:10 +0900

    home page

---
+++ components/GlitchText/index.js
@@ -0,0 +1,13 @@
+import style from './Glitch.module.css'
+
+export default function GlitchText({ name }) {
+  return (
+    <div className="text-4xl">
+      <h1 className={style.glitch}>
+        <span aria-hidden="true">{name}</span>
+        {name}
+        <span aria-hidden="true">{name}</span>
+      </h1>
+    </div>
+  )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,78 @@
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' }
+]
+
+const NavBar = () => {
+  return (
+    <Popover>
+      <div className="relative px-4 pt-6 sm:px-6 lg:px-8">
+        <nav
+          className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+          aria-label="Global"
+        >
+          <div className="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
+            <div className="-mr-2 flex w-full items-center justify-end md:hidden">
+              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span className="sr-only">Open main menu</span>
+                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
+          </div>
+          <div className="hidden md:ml-10 md:block md:space-x-8 md:pr-4">
+            {navigation.map(item => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="font-medium text-gray-500 hover:text-gray-900"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </nav>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-150 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
+        >
+          <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+            <div className="-mr-2 flex items-center justify-end px-5 pt-4">
+              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span className="sr-only">Close main menu</span>
+                <XIcon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map(item => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  )
+}

4159877ce3dd1e739631b91684c0683511320cde
Marcuz Corpuz <macoycorpuz@gmail.com>
2022-02-17 19:32:17 +0900

    Hero section

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,33 @@
-import GlitchText from '../components/GlitchText'
+const ButtonLink = ({ children }) => {
+  return (
+    <a className="cursor-pointer rounded-lg border-2 border-sky-300 bg-transparent py-5 px-7 font-mono text-sky-200 hover:bg-sky-900">
+      {children}
+    </a>
+  )
+}
 
 export default function Home() {
   return (
-    <div className="relative overflow-hidden bg-white">
-      <div className="mx-auto max-w-7xl">
-        <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
-          <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div className="sm:text-center lg:text-left">
-              <div className="flex items-center">
-                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                  Hi, I{"'"}m
-                </h1>
-                <GlitchText name="Marcuz" />
-              </div>
-              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
-                Full Stack developer with 5 years of experience in Software
-                Development with a wide range of technologies from web and
-                mobile applications to cloud infrastructure using AWS.
-              </p>
-              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
-                I started in the tech industry as a freelancer while I was
-                studying Computer Engineering at Mapua University.
-              </p>
-              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
-                After 3 years as a freelancer, I moved to Japan and started
-                working with various diversified teams. I worked with one of the
-                largest investment banking company and participated in digital
-                transformation projects.
-              </p>
-              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
-                I’m currently looking for opportunities and projects that allows
-                to work fully remote anywhere.
-              </p>
-            </div>
-          </main>
+    <div className="relative overflow-hidden bg-neutral-900 px-6">
+      <section className="mx-auto flex min-h-screen max-w-4xl flex-col justify-center">
+        <h1 className="mb-5 font-mono font-normal leading-[23.4px] text-sky-200">
+          Hi, my name is
+        </h1>
+        <h2 className="mb-4 text-5xl font-bold text-white md:text-6xl lg:text-7xl">
+          Marcuz Corpuz.
+        </h2>
+        <h3 className="text-5xl font-semibold text-gray-400 md:text-6xl lg:text-7xl">
+          I build experiences for the people.
+        </h3>
+        <p className="mt-5 max-w-lg text-slate-300">
+          I&apos;m a software engineer specializing in building web and mobile
+          applications using a wide range of technologies. Currently, I&apos;m
+          focusing on building user friendly applications.
+        </p>
+        <div className="mt-12">
+          <ButtonLink>Check out my works</ButtonLink>
         </div>
-      </div>
+      </section>
     </div>
   )
 }

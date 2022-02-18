3cd0173bb188b385dc01876e0cfb8e739d065590
Marcuz Corpuz <macoycorpuz@gmail.com>
2022-02-19 04:26:26 +0900

    remove nav in home page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Navbar from '../components/Navbar'
 export default function Home() {
   return (
     <div className="relative overflow-hidden bg-neutral-900 px-6">
-      <Navbar />
       <Hero />
     </div>
   )
